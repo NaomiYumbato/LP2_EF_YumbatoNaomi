71b843b905eb9c5677bc1d1ee0e01bd36b5b2754
Naomi <yumbatolopeznaomi@gmail.com>
2024-07-03 17:17:14 -0500

    login creado e implementado

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -10,13 +10,21 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.entity.UsuarioEntity;
 import com.example.demo.service.UsuarioService;
-import com.example.demo.utils.Utils;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+
+	@GetMapping("/")
+	public String showHome(Model model) {
+		
+		return "home";
+	}
 	
 	@GetMapping("/registrar")
 	public String showRegistroUsuario(Model model) {
@@ -28,12 +36,25 @@ public class UsuarioController {
 	@PostMapping("/registrar")
 	public String registrarUsuario(UsuarioEntity usuarioEntity, Model model, 
 			@RequestParam("foto")MultipartFile foto) {
-		
-		String nombreFoto = Utils.guardarImagen(foto);
-		usuarioEntity.setUrlImagen(nombreFoto);
-		
 		usuarioService.crearUsuario(usuarioEntity, model, foto);
 		
 		return "registrar_usuario";
+	}
+	
+	@GetMapping("/login")
+	public String showLogin(Model model) {
+		model.addAttribute("usuario", new UsuarioEntity());
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String login(UsuarioEntity usuarioEntity, Model model, HttpSession session) {
+		boolean usuarioValido = usuarioService.validarUsuario(usuarioEntity, session);
+		if(usuarioValido) {
+			return "redirect:/mantenerProducto";
+		}
+		model.addAttribute("loginInvalido", "Usuario no encontrado");
+		model.addAttribute("usuario", new UsuarioEntity());
+		return "login";
 	}
 }
--- src/main/java/com/example/demo/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UsuarioServiceImpl.java
@@ -8,29 +8,50 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.demo.entity.UsuarioEntity;
 import com.example.demo.repository.UsuarioRepository;
 import com.example.demo.service.UsuarioService;
+import com.example.demo.utils.Utils;
 
 import jakarta.servlet.http.HttpSession;
 
 @Service
-public class UsuarioServiceImpl implements UsuarioService{
-	
+public class UsuarioServiceImpl implements UsuarioService {
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
 	@Override
 	public void crearUsuario(UsuarioEntity usuarioEntity, Model model, MultipartFile foto) {
-				
-				usuarioRepository.save(usuarioEntity);
-				
-				model.addAttribute("registroCorrecto", "Usuario registrado correctamente");
-				model.addAttribute("usuario", new UsuarioEntity());
 		
+		String nombreFoto = Utils.guardarImagen(foto);
+		usuarioEntity.setUrlImagen(nombreFoto);
+
+		String passwordHash = Utils.extraerHash(usuarioEntity.getPassword());
+		usuarioEntity.setPassword(passwordHash);
+
+		usuarioRepository.save(usuarioEntity);
+
+		model.addAttribute("registroCorrecto", "Usuario registrado correctamente");
+		model.addAttribute("usuario", new UsuarioEntity());
+
 	}
 
 	@Override
 	public boolean validarUsuario(UsuarioEntity usuarioEntity, HttpSession session) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		UsuarioEntity usuarioEncontradoPorCorreo = 
+				usuarioRepository.findById(usuarioEntity.getCorreo()).get();
+		
+		if(usuarioEncontradoPorCorreo == null) {
+			return false;
+		}
+		
+		if(!Utils.checkPassword(usuarioEntity.getPassword(), 
+				usuarioEncontradoPorCorreo.getPassword())) {
+			return false;
+		}
+		
+		session.setAttribute("usuario", usuarioEncontradoPorCorreo.getCorreo());
+		
+		return true;
 	}
 
 	@Override
